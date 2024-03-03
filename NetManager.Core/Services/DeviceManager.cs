66b83bbf029adeb270bee4effd955acd87409d06
Soul-Remix <soulremix-dev@pm.me>
2024-03-03 11:12:40 +0300

    Add device manager

---
+++ NetManager.Core/Services/DeviceManager.cs
@@ -0,0 +1,28 @@
+﻿using SharpPcap;
+using SharpPcap.LibPcap;
+
+namespace NetManager.Core.Services;
+
+public class DeviceManager
+{
+    private string GetAdapterName(string? adapterName = null)
+    {
+        return (from device in LibPcapLiveDeviceList.Instance
+                where device.Interface.FriendlyName == (adapterName ?? HostInfo.NetworkAdapterName)
+                select device).ToList()[0].Name;
+    }
+
+    public LibPcapLiveDevice CreateDevice(
+        string filter,
+        PacketArrivalEventHandler? packetArrivalHandler,
+        int readTimeout = 1000,
+        string? adapterName = null)
+    {
+        var device = LibPcapLiveDeviceList.New()[GetAdapterName(adapterName)];
+        device.Open(DeviceModes.Promiscuous, readTimeout);
+        device!.Filter = filter;
+        device.OnPacketArrival += packetArrivalHandler;
+
+        return device;
+    }
+}
