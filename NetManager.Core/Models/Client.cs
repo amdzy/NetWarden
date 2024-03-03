eadb940df62988daf9e0cd1b0e577b002165aa38
Soul-Remix <soulremix-dev@pm.me>
2024-03-03 11:12:26 +0300

    Add client model

---
+++ NetManager.Core/Models/Client.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using NetManager.Core.Services;
+
+namespace NetManager.Core.Models;
+
+public class Client
+{
+    public IPAddress Ip { get; }
+    public PhysicalAddress Mac { get; }
+    public DateTime DateAdded { get; } = DateTime.UtcNow;
+    public bool HasFriendlyName { get; private set; }
+    public bool IsKilled { get; private set; }
+    public string Name { get; private set; }
+    public string Vendor { get; private set; }
+    public string Type { get; private set; }
+    public DateTime LastArpTime { get; private set; } = DateTime.UtcNow;
+    public bool IsOnline { get; private set; }
+
+    public Client(IPAddress ip, PhysicalAddress mac)
+    {
+        Ip = ip;
+        Mac = mac;
+        Name = mac.ToString();
+        Type = "Unknown";
+        Vendor = "Unknown";
+        IsOnline = true;
+    }
+
+    public void SetIsOnline() => IsOnline = true;
+    public void SetIsOffline() => IsOnline = false;
+    public void UpdateLastArpTime() => LastArpTime = DateTime.UtcNow;
+    public bool IsGateway() => Mac!.Equals(HostInfo.HostMac);
+    public bool IsLocalDevice() => Mac!.Equals(HostInfo.HostMac);
+}
