839fb070b24874be78ecdf5a6191a98d68a436b6
Soul-Remix <soulremix-dev@pm.me>
2024-03-03 11:21:12 +0300

    Implement host info

---
+++ NetManager.Core/Services/HostInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using NetManager.Core.Helpers;
+using SharpPcap.LibPcap;
+
+namespace NetManager.Core.Services;
+
+public class HostInfo
+{
+    public static IPAddress? HostIp { get; private set; }
+    public static PhysicalAddress? HostMac { get; private set; }
+    public static string? NetworkAdapterName { get; private set; }
+
+    public static void SetHostInfo(LibPcapLiveDevice device)
+    {
+        foreach (var addr in device.Addresses)
+        {
+            if (addr.Addr.ipAddress != null)
+            {
+                HostIp = addr.Addr.ipAddress;
+                HostMac = addr.Addr.hardwareAddress;
+                break;
+            }
+        }
+        NetworkAdapterName = device.Interface.FriendlyName;
+    }
+
+    public static string? RootIp => DataHelpers.GetRootIp(HostIp!);
+    public readonly static PhysicalAddress BroadcastMac = PhysicalAddress.Parse("FF-FF-FF-FF-FF-FF");
+    public readonly static PhysicalAddress EmptyMac = PhysicalAddress.Parse("00-00-00-00-00-00");
+
+}
