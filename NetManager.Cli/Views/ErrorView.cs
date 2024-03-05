46c5311a999e40f3e05239ee8313f4b9135c75ff
Soul-Remix <soulremix-dev@pm.me>
2024-03-05 15:19:40 +0300

    Implement cli error view

---
+++ NetManager.Cli/Views/ErrorView.cs
@@ -0,0 +1,32 @@
+namespace NetManager.Cli.Views
+{
+    using NetManager.Core.Exceptions;
+    using Terminal.Gui;
+
+
+    public partial class ErrorView
+    {
+
+        public ErrorView(Exception ex)
+        {
+            InitializeComponent();
+
+            errorMessage.Text = ex.Message;
+            if (ex is PermissionDeniedException)
+            {
+                errorTitle.Text = "Permission Denied";
+            }
+            if (ex is PlatformNotSupportedException)
+            {
+                errorTitle.Text = "Platform Not Supported";
+            }
+
+            exitBtn.Clicked += OnExitBtn;
+        }
+
+        public void OnExitBtn()
+        {
+            Application.RequestStop();
+        }
+    }
+}
