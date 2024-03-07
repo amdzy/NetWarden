37aa2fb0658d8f395ba93ab6d9a7fb52842f1dd1
Soul-Remix <soulremix-dev@pm.me>
2024-03-07 10:13:56 +0300

    Implement main view in cli

---
+++ NetManager.Cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using NetManager.Cli.Controllers;
+using NetManager.Cli.Views;
+using Terminal.Gui;
+
+namespace NetManager.Cli;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Application.Init();
+
+            var netManager = new Core.NetManager();
+            var controller = new MainController(netManager);
+            controller.Start();
+
+            Application.Run(new MainView(controller));
+        }
+        catch (Exception ex)
+        {
+            Application.Run(new ErrorView(ex));
+        }
+        finally
+        {
+            Application.Shutdown();
+        }
+
+    }
+}
---
+++ NetManager.Cli/Views/MainView.cs
@@ -0,0 +1,127 @@
+namespace NetManager.Cli.Views
+{
+    using System.Collections.Specialized;
+    using NetManager.Cli.Controllers;
+    using NetManager.Core.Exceptions;
+    using Terminal.Gui;
+
+    public partial class MainView
+    {
+        private MainController _mainController;
+
+        public MainView(MainController mainController)
+        {
+            InitializeComponent();
+            _mainController = mainController;
+
+            _mainController.Clients.CollectionChanged += OnCollectionChanged;
+
+            killBtn.Clicked += OnKillBtn;
+            unKillBtn.Clicked += OnUnKillBtn;
+            unKillAllBtn.Clicked += OnUnKillAllBtn;
+            killAllBtn.Clicked += OnKillAllBtn;
+            refreshBtn.Clicked += OnRefreshBtn;
+            exitBtn.Clicked += OnExitBtn;
+
+            refreshBtn.Visible = false;
+
+            tableView.SelectedCellChanged += (TableView.SelectedCellChangedEventArgs e) =>
+            {
+                UpdateButtons();
+            };
+
+            UpdateButtons();
+            Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                refreshBtn.Visible = true;
+            });
+        }
+
+        public void OnKillBtn()
+        {
+            _mainController.KillClient(tableView.SelectedRow);
+            UpdateButtons();
+        }
+
+        public void OnUnKillBtn()
+        {
+            _mainController.UnKillClient(tableView.SelectedRow);
+            UpdateButtons();
+        }
+
+        public void OnUnKillAllBtn()
+        {
+            _mainController.UnKilAllClients();
+            UpdateButtons();
+        }
+
+        public void OnKillAllBtn()
+        {
+            _mainController.KilAllClients();
+            UpdateButtons();
+        }
+
+        public void OnRefreshBtn()
+        {
+            refreshBtn.Visible = false;
+            UpdateTable();
+            UpdateButtons();
+            Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                refreshBtn.Visible = true;
+            });
+        }
+
+        public void UpdateTable()
+        {
+            var dt = _mainController.GetTableData();
+
+            tableView.Table = dt;
+            tableView.Update();
+        }
+
+        public void UpdateButtons()
+        {
+            if (_mainController.Clients.Count == 0)
+            {
+                killBtn.Visible = false;
+                unKillBtn.Visible = false;
+                unKillAllBtn.Visible = false;
+                killAllBtn.Visible = false;
+                return;
+            }
+            else
+            {
+                unKillAllBtn.Visible = true;
+                killAllBtn.Visible = true;
+            }
+            var row = tableView.SelectedRow;
+            var client = _mainController.Clients?[row];
+            if (client is not null)
+            {
+                if (client.IsKilled)
+                {
+                    killBtn.Visible = false;
+                    unKillBtn.Visible = true;
+                }
+                else
+                {
+                    killBtn.Visible = true;
+                    unKillBtn.Visible = false;
+                }
+            }
+        }
+
+        public void OnExitBtn()
+        {
+            _mainController.Exit();
+        }
+
+        private void OnCollectionChanged(object? obj, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateTable();
+        }
+    }
+}
