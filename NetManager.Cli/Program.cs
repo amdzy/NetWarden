544484710a4717dffcdafac6b3c7a9cd7f67809f
Soul-Remix <soulremix-dev@pm.me>
2024-03-12 13:22:58 +0300

    Implement parsing line args and help command

--- NetManager.Cli/Program.cs
+++ NetManager.Cli/Program.cs
@@ -1,4 +1,5 @@
-﻿using NetManager.Cli.Controllers;
+﻿using System.Reflection;
+using NetManager.Cli.Controllers;
 using NetManager.Cli.Views;
 using Terminal.Gui;
 
@@ -10,6 +11,12 @@ public class Program
     {
         try
         {
+            if (args.Length > 0)
+            {
+                HandleCommandLineArgs(args);
+                return;
+            }
+
             Application.Init();
 
             var netManager = new Core.NetManager();
@@ -27,5 +34,33 @@ public class Program
             Application.Shutdown();
         }
 
+    }
+
+    public static void HandleCommandLineArgs(string[] args)
+    {
+        switch (args[0])
+        {
+            case "help" or "--h" or "-h":
+                PrintHelp();
+                break;
+        }
+    }
+
+    static void PrintHelp()
+    {
+        Console.WriteLine("NetManager - Network Management TUI Tool");
+        Console.WriteLine("Usage: netmanager");
+        Console.WriteLine();
+        Console.WriteLine("Keyboard Shortcuts Inside the TUI:");
+        Console.WriteLine("  Alt + R   Refresh");
+        Console.WriteLine("  Alt + K   Kill (Cut) a specific device");
+        Console.WriteLine("  Alt + S   Restore a specific device");
+        Console.WriteLine("  Alt + A   Kill (Cut) all devices");
+        Console.WriteLine("  Alt + D   Restore all devices");
+        Console.WriteLine("  Alt + U   Update the name for a device");
+        Console.WriteLine("  Alt/Ctrl + Q   Quit the TUI");
+        Console.WriteLine("Options:");
+        Console.WriteLine("  -h, --help     Show this help information");
+        Console.WriteLine("  -v, --version  Show the version information");
     }
 }
