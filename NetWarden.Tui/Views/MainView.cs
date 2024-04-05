441642c34926fe436379b81a903a8afda5ec1dec
Soul-Remix <soulremix-dev@pm.me>
2024-04-05 15:19:16 +0300

    Fix migration error in main view

--- NetWarden.Tui/Views/MainView.cs
+++ NetWarden.Tui/Views/MainView.cs
@@ -1,161 +1,162 @@
-namespace NetWarden.Tui.Views
+using System.ComponentModel;
+using NetWarden.Tui.Controllers;
+using Terminal.Gui;
+
+namespace NetWarden.Tui.Views;
+
+
+public partial class MainView
 {
-    using NetWarden.Tui.Controllers;
-    using Terminal.Gui;
+    private MainController _mainController;
 
-    public partial class MainView
+    public MainView(MainController mainController)
     {
-        private MainController _mainController;
+        InitializeComponent();
+        _mainController = mainController;
 
-        public MainView(MainController mainController)
+        _mainController.ClientsChanged += OnCollectionChanged;
+
+        killBtn.Accept += OnKillBtn;
+        unKillBtn.Accept += OnUnKillBtn;
+        unKillAllBtn.Accept += OnUnKillAllBtn;
+        killAllBtn.Accept += OnKillAllBtn;
+        refreshBtn.Accept += OnRefreshBtn;
+        exitBtn.Accept += OnExitBtn;
+
+        openNameViewBtn.Accept += OnOpenNameView;
+        cancelSetNameBtn.Accept += OnCloseNameView;
+        setNameBtn.Accept += OnSetNameBtn;
+
+        updateInterfaceBtn.Accept += OnUpdateInterface;
+
+        refreshBtn.Visible = false;
+
+        tableView.SelectedCellChanged += (object? sender, SelectedCellChangedEventArgs e) =>
         {
-            InitializeComponent();
-            _mainController = mainController;
-
-            _mainController.ClientsChanged += OnCollectionChanged;
-
-            killBtn.Clicked += OnKillBtn;
-            unKillBtn.Clicked += OnUnKillBtn;
-            unKillAllBtn.Clicked += OnUnKillAllBtn;
-            killAllBtn.Clicked += OnKillAllBtn;
-            refreshBtn.Clicked += OnRefreshBtn;
-            exitBtn.Clicked += OnExitBtn;
-
-            openNameViewBtn.Clicked += OnOpenNameView;
-            cancelSetNameBtn.Clicked += OnCloseNameView;
-            setNameBtn.Clicked += OnSetNameBtn;
-
-            updateInterfaceBtn.Clicked += OnUpdateInterface;
-
-            refreshBtn.Visible = false;
-
-            tableView.SelectedCellChanged += (TableView.SelectedCellChangedEventArgs e) =>
-            {
-                UpdateButtons();
-            };
-
             UpdateButtons();
-            Task.Run(async () =>
-            {
-                await Task.Delay(2000);
-                refreshBtn.Visible = true;
-            });
+        };
 
-            killAllBtn.HotKey = Key.A;
-            unKillBtn.HotKey = Key.V;
-            unKillAllBtn.HotKey = Key.D;
-        }
-
-        public void OnKillBtn()
+        UpdateButtons();
+        Task.Run(async () =>
         {
-            _mainController.KillClient(tableView.SelectedRow);
-            UpdateButtons();
-        }
+            await Task.Delay(2000);
+            refreshBtn.Visible = true;
+        });
 
-        public void OnUnKillBtn()
+        var dt = _mainController.GetBaseTableData();
+
+        tableView.Table = new DataTableSource(dt);
+    }
+
+    public void OnKillBtn(object? sender, CancelEventArgs e)
+    {
+        _mainController.KillClient(tableView.SelectedRow);
+        UpdateButtons();
+    }
+
+    public void OnUnKillBtn(object? sender, CancelEventArgs e)
+    {
+        _mainController.UnKillClient(tableView.SelectedRow);
+        UpdateButtons();
+    }
+
+    public void OnUnKillAllBtn(object? sender, CancelEventArgs e)
+    {
+        _mainController.UnKilAllClients();
+        UpdateButtons();
+    }
+
+    public void OnKillAllBtn(object? sender, CancelEventArgs e)
+    {
+        _mainController.KilAllClients();
+        UpdateButtons();
+    }
+
+    public void OnRefreshBtn(object? sender, CancelEventArgs e)
+    {
+        refreshBtn.Visible = false;
+        UpdateTable();
+        UpdateButtons();
+        Task.Run(async () =>
         {
-            _mainController.UnKillClient(tableView.SelectedRow);
-            UpdateButtons();
-        }
+            await Task.Delay(2000);
+            refreshBtn.Visible = true;
+        });
+    }
 
-        public void OnUnKillAllBtn()
+    public void UpdateTable()
+    {
+        var dt = _mainController.GetTableData();
+
+        tableView.Table = new DataTableSource(dt);
+    }
+
+    public void UpdateButtons()
+    {
+        if (_mainController.Clients.Length == 0)
         {
-            _mainController.UnKilAllClients();
-            UpdateButtons();
+            killBtn.Visible = false;
+            unKillBtn.Visible = false;
+            unKillAllBtn.Visible = false;
+            killAllBtn.Visible = false;
+            openNameViewBtn.Visible = false;
+            return;
         }
-
-        public void OnKillAllBtn()
+        else
         {
-            _mainController.KilAllClients();
-            UpdateButtons();
+            unKillAllBtn.Visible = true;
+            killAllBtn.Visible = true;
         }
-
-        public void OnRefreshBtn()
+        var row = tableView.SelectedRow;
+        var client = _mainController.Clients?[row];
+        if (client is not null)
         {
-            refreshBtn.Visible = false;
-            UpdateTable();
-            UpdateButtons();
-            Task.Run(async () =>
-            {
-                await Task.Delay(2000);
-                refreshBtn.Visible = true;
-            });
-        }
-
-        public void UpdateTable()
-        {
-            var dt = _mainController.GetTableData();
-
-            tableView.Table = dt;
-            tableView.Update();
-        }
-
-        public void UpdateButtons()
-        {
-            if (_mainController.Clients.Length == 0)
+            if (client.IsKilled)
             {
                 killBtn.Visible = false;
-                unKillBtn.Visible = false;
-                unKillAllBtn.Visible = false;
-                killAllBtn.Visible = false;
-                openNameViewBtn.Visible = false;
-                return;
+                unKillBtn.Visible = true;
             }
             else
             {
-                unKillAllBtn.Visible = true;
-                killAllBtn.Visible = true;
+                killBtn.Visible = true;
+                unKillBtn.Visible = false;
             }
-            var row = tableView.SelectedRow;
-            var client = _mainController.Clients?[row];
-            if (client is not null)
-            {
-                if (client.IsKilled)
-                {
-                    killBtn.Visible = false;
-                    unKillBtn.Visible = true;
-                }
-                else
-                {
-                    killBtn.Visible = true;
-                    unKillBtn.Visible = false;
-                }
-                openNameViewBtn.Visible = true;
-            }
-        }
-
-        public void OnExitBtn()
-        {
-            _mainController.Exit();
-        }
-
-        public void OnOpenNameView()
-        {
-            newNameField.Text = "";
-            updateNameView.Visible = true;
-        }
-
-        public void OnCloseNameView()
-        {
-            updateNameView.Visible = false;
-        }
-
-        public void OnSetNameBtn()
-        {
-            var newName = newNameField.Text.ToString() ?? "";
-            _mainController.UpdateClientName(tableView.SelectedRow, newName);
-            OnCloseNameView();
-        }
-
-        public void OnUpdateInterface()
-        {
-            _mainController.UpdateInterface();
-        }
-
-
-        private void OnCollectionChanged(object? obj, EventArgs e)
-        {
-            UpdateTable();
+            openNameViewBtn.Visible = true;
         }
     }
+
+    public void OnExitBtn(object? sender, CancelEventArgs e)
+    {
+        _mainController.Exit();
+    }
+
+    public void OnOpenNameView(object? sender, CancelEventArgs e)
+    {
+        newNameField.Text = "";
+        updateNameView.Visible = true;
+    }
+
+    public void OnCloseNameView(object? sender, CancelEventArgs e)
+    {
+        updateNameView.Visible = false;
+    }
+
+    public void OnSetNameBtn(object? sender, CancelEventArgs e)
+    {
+        var newName = newNameField.Text.ToString() ?? "";
+        _mainController.UpdateClientName(tableView.SelectedRow, newName);
+        OnCloseNameView(sender, e);
+    }
+
+    public void OnUpdateInterface(object? sender, CancelEventArgs e)
+    {
+        _mainController.UpdateInterface();
+    }
+
+
+    private void OnCollectionChanged(object? obj, EventArgs e)
+    {
+        UpdateTable();
+    }
 }
+
