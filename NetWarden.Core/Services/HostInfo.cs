2eedd05b6830cb309793e13f8f1d8b7bec5ff641
amdzy <amdzy@proton.me>
2024-04-11 20:09:31 +0300

    Update host info to set gateway mac

--- NetWarden.Core/Services/HostInfo.cs
+++ NetWarden.Core/Services/HostInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using NetWarden.Core.Extensions;
+using SharpPcap;
 using SharpPcap.LibPcap;
 
 namespace NetWarden.Core.Services;
@@ -10,24 +12,41 @@ public class HostInfo
     public static IPAddress? HostIp { get; private set; }
     public static PhysicalAddress? HostMac { get; private set; }
     public static IPAddress? GatewayIp { get; private set; }
-    public static PhysicalAddress? GatewayMAc { get; private set; }
+    public static PhysicalAddress? GatewayMac { get; private set; }
     public static string? NetworkAdapterName { get; private set; }
     public static IPAddress? NetMask { get; private set; }
 
     public static void SetHostInfo(LibPcapLiveDevice device)
     {
-        foreach (var addr in device.Addresses)
+        foreach (var address in device.Addresses)
         {
-            if (addr.Addr.ipAddress != null)
+            if (address.Addr.type == Sockaddr.AddressTypes.AF_INET_AF_INET6 && address.Addr.ipAddress.AddressFamily == AddressFamily.InterNetwork)
             {
-                HostIp = addr.Addr.ipAddress;
-                NetMask = addr.Netmask.ipAddress;
+                HostIp = address.Addr.ipAddress;
+                NetMask = address.Netmask.ipAddress;
+                break;
+            }
+        }
+        if (HostIp == null)
+        {
+            HostIp = IPAddress.Parse("127.0.0.1");
+        }
+
+        foreach (var address in device.Addresses)
+        {
+            if (address.Addr.type == Sockaddr.AddressTypes.HARDWARE)
+            {
+                HostMac = address.Addr.hardwareAddress;
                 break;
             }
         }
         NetworkAdapterName = device.Interface.FriendlyName;
-        HostMac = device.MacAddress;
         GatewayIp = device.Interface.GatewayAddresses.Count > 0 ? device.Interface.GatewayAddresses[0] : null;
+        if (GatewayIp is not null)
+        {
+            var arp = new ARP(device);
+            GatewayMac = arp.Resolve(GatewayIp, HostIp, HostMac);
+        }
     }
 
     public static string? RootIp => HostIp!.GetRootIp();
